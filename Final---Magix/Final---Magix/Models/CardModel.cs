ceab27bfbe0582adcb97fc6cf61711c941c8b128
VanceWint17 <vancewinters30@gmail.com>
2023-08-08 10:36:17 -0400

    Created the models for card and trade-in. Properties should appear this time

--- Final---Magix/Final---Magix/Models/CardModel.cs
+++ Final---Magix/Final---Magix/Models/CardModel.cs
@@ -1,6 +1,29 @@
-﻿namespace Final___Magix.Models
+﻿using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.ComponentModel.DataAnnotations;
+
+namespace Final___Magix.Models
 {
-    public class Card_Model
+    public class CardModel
     {
+
+        [Required]
+        public string Name { get; set; }
+        
+        public int Id { get; set; }
+        [Required]
+        public string Condition { get; set; }
+        [Required]
+        public string Set { get; set; }
+
+        [Required]
+
+        public string Print { get; set; }
+        [Required]
+        public bool Foil { get; set; }
+
+        public int Price { get; set; }
+        
+        public string image { get; set; }
+           
     }
 }
--- Final---Magix/Final---Magix/Models/TradeInModel.cs
+++ Final---Magix/Final---Magix/Models/TradeInModel.cs
@@ -1,6 +1,17 @@
-﻿namespace Final___Magix.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final___Magix.Models
 {
-    public class TradeIn_Model
+    public class TradeInModel
     {
+        
+        public int Id { get; set; } 
+
+        public virtual IEnumerable<CardModel>? Cards { get; set; }
+
+        //public int price { get; set; }
+
+
+
     }
 }
