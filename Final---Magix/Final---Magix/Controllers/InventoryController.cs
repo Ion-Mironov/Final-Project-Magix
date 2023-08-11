ba4eb000caba2451f8ef86a679848e3b1338a62a
Ivon Mironov <mironov.ion@gmail.com>
2023-08-11 10:49:05 -0400

    Manually ddded InventoryController.cs

---
+++ Final---Magix/Final---Magix/Controllers/InventoryController.cs
@@ -0,0 +1,113 @@
+﻿using Final___Magix.DataContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http; // Include the IFormCollection namespace
+
+
+namespace Final___Magix.Controllers
+	{
+	public class InventoryController : Controller
+		{
+		private readonly CardContext _dbContext;
+		private readonly ScryfallApiClient _scryfallApiClient;
+
+		public InventoryController(CardContext dbContext, ScryfallApiClient scryfallApiClient)
+			{
+			_dbContext = dbContext;
+			_scryfallApiClient = scryfallApiClient;
+			}
+
+		public IActionResult BrowseInventory()
+			{
+			// Fetch inventory from your database
+			var inventory = _dbContext.StoreInventory.ToList();
+
+			// Fetch additional card data from Scryfall API
+			foreach (var item in inventory)
+				{
+				var scryfallApiResponse = _scryfallApiClient.GetCardByName(item.CName);
+				if (scryfallApiResponse != null)
+					{
+					// Update item with Scryfall data if needed
+					// For example: item.ScryfallImageUrl = scryfallApiResponse.ImageUrl;
+					}
+				}
+
+			return View(inventory);
+			}
+
+		// GET: InventoryController
+		public ActionResult Index()
+			{
+			return View();
+			}
+
+		// GET: InventoryController/Details/5
+		public ActionResult Details(int id)
+			{
+			return View();
+			}
+
+		// GET: InventoryController/Create
+		public ActionResult Create()
+			{
+			return View();
+			}
+
+		// POST: InventoryController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+			{
+			try
+				{
+				return RedirectToAction(nameof(Index));
+				}
+			catch
+				{
+				return View();
+				}
+			}
+
+		// GET: InventoryController/Edit/5
+		public ActionResult Edit(int id)
+			{
+			return View();
+			}
+
+		// POST: InventoryController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+			{
+			try
+				{
+				return RedirectToAction(nameof(Index));
+				}
+			catch
+				{
+				return View();
+				}
+			}
+
+		// GET: InventoryController/Delete/5
+		public ActionResult Delete(int id)
+			{
+			return View();
+			}
+
+		// POST: InventoryController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+			{
+			try
+				{
+				return RedirectToAction(nameof(Index));
+				}
+			catch
+				{
+				return View();
+				}
+			}
+		}
+	}
