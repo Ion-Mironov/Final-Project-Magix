d4e4578ef5e8d7038499b5620e9b903f47359464
Ivon Mironov <mironov.ion@gmail.com>
2023-08-11 11:05:14 -0400

    Added in InventoryModel.cs

---
+++ Final---Magix/Final---Magix/Models/InventoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final___Magix.Models
+	{
+	public class InventoryModel
+		{
+		[Required]
+		public int Id { get; set; }
+		public string CName { get; set; }
+		[Required]
+		public string CImageUrl { get; set; }
+		public decimal Price { get; set; }
+		}
+	}
