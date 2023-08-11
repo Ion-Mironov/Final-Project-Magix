121d601d6fd4fed145a7477ef98e6d7e399bb73c
Ivon Mironov <mironov.ion@gmail.com>
2023-08-11 18:13:15 -0400

    Addtional style to Inventory page. Same for site.css.

---
+++ Final---Magix/Final---Magix/Controllers/InventoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final___Magix.Controllers
+	{
+	public class InventoryController : Controller
+		{
+		public IActionResult Index()
+			{
+			return View();
+			}
+		}
+	}
