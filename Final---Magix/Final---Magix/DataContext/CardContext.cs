69f2ffbfea7fcc1196cd59d32d5f164dc0915078
Ivon Mironov <mironov.ion@gmail.com>
2023-08-14 11:19:49 -0400

    Added additional folders/files for testing.

--- Final---Magix/Final---Magix/DataContext/CardContext.cs
+++ Final---Magix/Final---Magix/DataContext/CardContext.cs
@@ -1,4 +1,25 @@
-﻿using Final___Magix.Models;
+﻿//using Final___Magix.Models;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace Final___Magix.DataContext
+//	{
+//	public class CardContext : DbContext
+//		{
+//		private const string CONNECTION_STRING = "Server=(localdb)\\mssqllocaldb;Database=Final---Magix;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+//		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//			{
+//			optionsBuilder.UseSqlServer(CONNECTION_STRING);
+//			}
+//		public DbSet<CardModel> Cards { get; set; }
+
+//		public DbSet<TradeInModel> TradeIns { get; set; }
+
+//		}
+//	}
+
+
+using Final___Magix.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Final___Magix.DataContext
@@ -11,10 +32,30 @@ namespace Final___Magix.DataContext
 			{
 			optionsBuilder.UseSqlServer(CONNECTION_STRING);
 			}
-		public DbSet<CardModel> Cards { get; set; }
+		public DbSet<CardModel> Cards { get; set; }// represents the card collection
 
-		public DbSet<TradeInModel> TradeIns { get; set; }
+		public DbSet<TradeInModel> TradeIns { get; set; }// represent trades
+
+		public DbSet<InventoryModel> StoreInventory { get; set; }
 
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+			{
+			// Seed initial store inventory data
+			modelBuilder.Entity<InventoryModel>().HasData(
+				new InventoryModel { Id = 1, Name = "Card A", ImageUrl = "url", Price = 5.99m },
+				new InventoryModel { Id = 2, Name = "Card B", ImageUrl = "url", Price = 3.49m }
+			// Add more items as needed
+			);
+
+			// Specify the column type, precision, and scale for the Price property
+			modelBuilder.Entity<InventoryModel>()
+				.Property(i => i.Price)
+				.HasColumnType("decimal(6, 2)"); // Adjust precision and scale as needed. Might need to be lowered.
+			modelBuilder.Entity<TradeInModel>().HasData(
+				new TradeInModel { Id = 1 },
+				new TradeInModel { Id = 2 }
+				);
+			}
 		}
 	}
--- Final---Magix/Final---Magix/Models/InventoryModel.cs
+++ Final---Magix/Final---Magix/Models/InventoryModel.cs
@@ -4,13 +4,20 @@ namespace Final___Magix.Models
 	{
 	public class InventoryModel
 		{
+		[Key]
+		public int Id { get; set; }
+
 		[Required]
 		public string Name { get; set; }
+
 		[Required]
 		public string ImageUrl { get; set; }
+
 		[Required]
 		public decimal Price { get; set; }
+
 		[Required]
 		public int Quantity { get; set; }
+
 		}
 	}
