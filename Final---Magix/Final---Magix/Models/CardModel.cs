e3d91d1b4e048d002793e20e085cef5736b93000
Ivon Mironov <mironov.ion@gmail.com>
2023-08-14 11:47:03 -0400

    Updated CardModel.cs

--- Final---Magix/Final---Magix/Models/CardModel.cs
+++ Final---Magix/Final---Magix/Models/CardModel.cs
@@ -4,23 +4,25 @@ namespace Final___Magix.Models
 	{
 	public class CardModel
 		{
-		public int Id { get; set; }
-		public int Price { get; set; }
-		public string Image { get; set; }
+		[Key]
+		public int Id { get; set; }				// Name of the card
+
+		public decimal Price { get; set; }		// Card's worth
+		public string Image { get; set; }		// URL for the card picture
 
 		[Required]
-		public string Name { get; set; }
+		public string Name { get; set; }		// Unique identifier for each card
 
 		[Required]
 		public string Condition { get; set; }
 
 		[Required]
-		public string Set { get; set; }
+		public string Set { get; set; }         // Which group the card belongs to
 
 		[Required]
-		public string Print { get; set; }
+		public string Print { get; set; }		// Regular or Special printing
 
 		[Required]
-		public bool Foil { get; set; }
+		public bool Foil { get; set; }			// Whether the card is shiny or not
 		}
 	}
