cd8dcd030eaf302c2386adf35a93dd4903cfdbe0
Matthew Elder <Matthew.elder@hotmail.com>
2023-08-16 13:55:30 -0400

    Format of BulkData using JsonPropertyName tag Fixed name convention issues

--- Final---Magix/Final---Magix/Models/BulkDataModel.cs
+++ Final---Magix/Final---Magix/Models/BulkDataModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Final___Magix.Models
 {
@@ -12,130 +13,232 @@ namespace Final___Magix.Models
 
         public class BulkDataRoot
         {
-            public string _object { get; set; }
-            public string id { get; set; }
-            public string oracle_id { get; set; }
-            public int[] multiverse_ids { get; set; }
-            public int mtgo_id { get; set; }
-            public int mtgo_foil_id { get; set; }
-            public int tcgplayer_id { get; set; }
-            public int cardmarket_id { get; set; }
-            public string name { get; set; }
-            public string lang { get; set; }
-            public string released_at { get; set; }
-            public string uri { get; set; }
-            public string scryfall_uri { get; set; }
-            public string layout { get; set; }
-            public bool highres_image { get; set; }
-            public string image_status { get; set; }
-            public Image_Uris image_uris { get; set; }
-            public string mana_cost { get; set; }
-            public float cmc { get; set; }
-            public string type_line { get; set; }
-            public string oracle_text { get; set; }
-            public object[] colors { get; set; }
-            public object[] color_identity { get; set; }
-            public object[] keywords { get; set; }
-            public Legalities legalities { get; set; }
-            public string[] games { get; set; }
-            public bool reserved { get; set; }
-            public bool foil { get; set; }
-            public bool nonfoil { get; set; }
-            public string[] finishes { get; set; }
-            public bool oversized { get; set; }
-            public bool promo { get; set; }
-            public bool reprint { get; set; }
-            public bool variation { get; set; }
+            [JsonPropertyName("object")]
+            public string Object { get; set; }
+            [JsonPropertyName("id")]
+            [Key]
+            public string Id { get; set; }
+            [JsonPropertyName("oracle_id")]
+            public string OracleId { get; set; }
+            [JsonPropertyName("multiverse_ids")]
+            public int[] MultiverseIds { get; set; }
+            [JsonPropertyName("mtgo_id")]
+            public int MtgoId { get; set; }
+            [JsonPropertyName("mtgo_foil_id")]
+            public int MtgoFoilId { get; set; }
+            [JsonPropertyName("tcgplayer_id")]
+            public int TcgplayerId { get; set; }
+            [JsonPropertyName("cardmarket_id")]
+            public int CardmarketId { get; set; }
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+            [JsonPropertyName("lang")]
+            public string Lang { get; set; }
+            [JsonPropertyName("released_at")]
+            public string ReleasedAt { get; set; }
+            [JsonPropertyName("uri")]
+            public string Uri { get; set; }
+            [JsonPropertyName("scryfall_uri")]
+            public string ScryfallUri { get; set; }
+            [JsonPropertyName("layout")]
+            public string Layout { get; set; }
+            [JsonPropertyName("highres_image")]
+            public bool HighresImage { get; set; }
+            [JsonPropertyName("image_status")]
+            public string ImageStatus { get; set; }
+            [JsonPropertyName("image_uris")]
+            public Image_Uris ImageUris { get; set; }
+            [JsonPropertyName("mana_cost")]
+            public string ManaCost { get; set; }
+            [JsonPropertyName("cmc")]
+            public float Cmc { get; set; }
+            [JsonPropertyName("type_line")]
+            public string TypeLine { get; set; }
+            [JsonPropertyName("oracle_text")]
+            public string OracleText { get; set; }
+            [JsonPropertyName("colors")]
+            public object[] Colors { get; set; }
+            [JsonPropertyName("color_identity")]
+            public object[] ColorIdentity { get; set; }
+            [JsonPropertyName("keywords")]
+            public object[] Keywords { get; set; }
+            [JsonPropertyName("legalities")]
+            public Legalities Legalities { get; set; }
+            [JsonPropertyName("games")]
+            public string[] Games { get; set; }
+            [JsonPropertyName("reserved")]
+            public bool Reserved { get; set; }
+            [JsonPropertyName("foil")]
+            public bool Foil { get; set; }
+            [JsonPropertyName("nonfoil")]
+            public bool Nonfoil { get; set; }
+            [JsonPropertyName("finishes")]
+            public string[] Finishes { get; set; }
+            [JsonPropertyName("oversized")]
+            public bool Oversized { get; set; }
+            [JsonPropertyName("promo")]
+            public bool Promo { get; set; }
+            [JsonPropertyName("reprint")]
+            public bool Reprint { get; set; }
+            [JsonPropertyName("variation")]
+            public bool Variation { get; set; }
             [JsonPropertyName("set_id")]
-            public string setId { get; set; }
-            public string set { get; set; }
+            public string SetId { get; set; }
+            [JsonPropertyName("set")]
+            public string Set { get; set; }
             [JsonPropertyName("set_name")]
-            public string setName { get; set; }
-            public string set_type { get; set; }
+            public string SetName { get; set; }
+            [JsonPropertyName("set_type")]
+            public string SetType { get; set; }
             [JsonPropertyName("set_uri")]
             public string setUri { get; set; }
-            public string set_search_uri { get; set; }
-            public string scryfall_set_uri { get; set; }
-            public string rulings_uri { get; set; }
-            public string prints_search_uri { get; set; }
-            public string collector_number { get; set; }
-            public bool digital { get; set; }
-            public string rarity { get; set; }
-            public string flavor_text { get; set; }
-            public string card_back_id { get; set; }
-            public string artist { get; set; }
-            public string[] artist_ids { get; set; }
-            public string illustration_id { get; set; }
-            public string border_color { get; set; }
-            public string frame { get; set; }
-            public bool full_art { get; set; }
-            public bool textless { get; set; }
-            public bool booster { get; set; }
-            public bool story_spotlight { get; set; }
-            public int edhrec_rank { get; set; }
-            public Prices prices { get; set; }
-            public Related_Uris related_uris { get; set; }
-            public Purchase_Uris purchase_uris { get; set; }
+            [JsonPropertyName("set_search_uri")]
+            public string SetSearchUri { get; set; }
+            [JsonPropertyName("scryfall_set_uri")]
+            public string ScryfallSetUri { get; set; }
+            [JsonPropertyName("rulings_uri")]
+            public string RulingsUri { get; set; }
+            [JsonPropertyName("prints_search_uri")]
+            public string PrintsSearchUri { get; set; }
+            [JsonPropertyName("collector_number")]
+            public string CollectorNumber { get; set; }
+            [JsonPropertyName("digital")]
+            public bool Digital { get; set; }
+            [JsonPropertyName("rarity")]
+            public string Rarity { get; set; }
+            [JsonPropertyName("flavor_text")]
+            public string FlavorText { get; set; }
+            [JsonPropertyName("card_back_id")]
+            public string CardBackId { get; set; }
+            [JsonPropertyName("artist")]
+            public string Artist { get; set; }
+            [JsonPropertyName("artist_ids")]
+            public string[] ArtistIds { get; set; }
+            [JsonPropertyName("illustration_id")]
+            public string IllustrationId { get; set; }
+            [JsonPropertyName("border_color")]
+            public string BorderColor { get; set; }
+            [JsonPropertyName("frame")]
+            public string Frame { get; set; }
+            [JsonPropertyName("full_art")]
+            public bool FullArt { get; set; }
+            [JsonPropertyName("textless")]
+            public bool Textless { get; set; }
+            [JsonPropertyName("booster")]
+            public bool Booster { get; set; }
+            [JsonPropertyName("story_spotlight")]
+            public bool StorySpotlight { get; set; }
+            [JsonPropertyName("edhrec_rank")]
+            public int EdhrecRank { get; set; }
+            [JsonPropertyName("prices")]
+            public Prices Prices { get; set; }
+            [JsonPropertyName("related_uris")]
+            public Related_Uris RelatedUris { get; set; }
+            [JsonPropertyName("purchase_uris")]
+            public Purchase_Uris PurchaseUris { get; set; }
+            [JsonPropertyName("")]
         }
 
         public class Image_Uris
         {
-            public string small { get; set; }
-            public string normal { get; set; }
-            public string large { get; set; }
-            public string png { get; set; }
-            public string art_crop { get; set; }
-            public string border_crop { get; set; }
+            [JsonPropertyName("small")]
+            public string Small { get; set; }
+            [JsonPropertyName("normal")]
+            public string Normal { get; set; }
+            [JsonPropertyName("large")]
+            public string Large { get; set; }
+            [JsonPropertyName("png")]
+            public string Png { get; set; }
+            [JsonPropertyName("art_crop")]
+            public string ArtCrop { get; set; }
+            [JsonPropertyName("border_crop")]
+            public string BorderCrop { get; set; }
+
         }
 
         public class Legalities
         {
-            public string standard { get; set; }
-            public string future { get; set; }
-            public string historic { get; set; }
-            public string gladiator { get; set; }
-            public string pioneer { get; set; }
-            public string explorer { get; set; }
-            public string modern { get; set; }
-            public string legacy { get; set; }
-            public string pauper { get; set; }
-            public string vintage { get; set; }
-            public string penny { get; set; }
-            public string commander { get; set; }
-            public string oathbreaker { get; set; }
-            public string brawl { get; set; }
-            public string historicbrawl { get; set; }
-            public string alchemy { get; set; }
-            public string paupercommander { get; set; }
-            public string duel { get; set; }
-            public string oldschool { get; set; }
-            public string premodern { get; set; }
-            public string predh { get; set; }
+            [JsonPropertyName("standard")]
+            public string Standard { get; set; }
+            [JsonPropertyName("future")]
+            public string Future { get; set; }
+            [JsonPropertyName("historic")]
+            public string Historic { get; set; }
+            [JsonPropertyName("gladiator")]
+            public string Gladiator { get; set; }
+            [JsonPropertyName("pioneer")]
+            public string Pioneer { get; set; }
+            [JsonPropertyName("explorer")]
+            public string Explorer { get; set; }
+            [JsonPropertyName("modern")]
+            public string Modern { get; set; }
+            [JsonPropertyName("legacy")]
+            public string Legacy { get; set; }
+            [JsonPropertyName("pauper")]
+            public string Pauper { get; set; }
+            [JsonPropertyName("vintage")]
+            public string Vintage { get; set; }
+            [JsonPropertyName("penny")]
+            public string Penny { get; set; }
+            [JsonPropertyName("commander")]
+            public string Commander { get; set; }
+            [JsonPropertyName("oathbreaker")]
+            public string Oathbreaker { get; set; }
+            [JsonPropertyName("brawl")]
+            public string Brawl { get; set; }
+            [JsonPropertyName("historicbrawl")]
+            public string HistoricBrawl { get; set; }
+            [JsonPropertyName("alchemy")]
+            public string Alchemy { get; set; }
+            [JsonPropertyName("paupercommander")]
+            public string PauperCommander { get; set; }
+            [JsonPropertyName("duel")]
+            public string Duel { get; set; }
+            [JsonPropertyName("oldschool")]
+            public string Oldschool { get; set; }
+            [JsonPropertyName("premodern")]
+            public string Premodern { get; set; }
+            [JsonPropertyName("predh")]
+            public string Predh { get; set; }
         }
 
         public class Prices
         {
-            public string usd { get; set; }
-            public object usd_foil { get; set; }
-            public object usd_etched { get; set; }
-            public string eur { get; set; }
-            public object eur_foil { get; set; }
-            public string tix { get; set; }
+            [JsonPropertyName("usd")]
+            public string Usd { get; set; }
+            [JsonPropertyName("usd_foil")]
+            public object UsdFoil { get; set; }
+            [JsonPropertyName("usd_etched")]
+            public object UsdEtched { get; set; }
+            [JsonPropertyName("eur")]
+            public string Eur { get; set; }
+            [JsonPropertyName("eur_foil")]
+            public object EurFoil { get; set; }
+            [JsonPropertyName("tix")]
+            public string Tix { get; set; }
         }
 
         public class Related_Uris
         {
-            public string gatherer { get; set; }
-            public string tcgplayer_infinite_articles { get; set; }
-            public string tcgplayer_infinite_decks { get; set; }
-            public string edhrec { get; set; }
+            [JsonPropertyName("gatherer")]
+            public string Gatherer { get; set; }
+            [JsonPropertyName("tcgplayer_infinite_articles")]
+            public string TcgplayerInfiniteArticles { get; set; }
+            [JsonPropertyName("tcgplayer_infinite_decks")]
+            public string TcgplayerInfiniteDecks { get; set; }
+            [JsonPropertyName("edhrec")]
+            public string Edhrec { get; set; }
+
         }
 
         public class Purchase_Uris
         {
-            public string tcgplayer { get; set; }
-            public string cardmarket { get; set; }
-            public string cardhoarder { get; set; }
+            [JsonPropertyName("tcgplayer")]
+            public string Tcgplayer { get; set; }
+            [JsonPropertyName("cardmarket")]
+            public string Cardmarket { get; set; }
+            [JsonPropertyName("cardhoarder")]
+            public string Cardhoarder { get; set; }
         }
 
     }
