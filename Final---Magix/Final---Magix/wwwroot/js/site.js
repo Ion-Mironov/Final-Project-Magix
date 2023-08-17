d7e76e91302ec9b6b0e7fda874047dd96a4e01b4
Ivon Mironov <mironov.ion@gmail.com>
2023-08-16 20:33:36 -0400

    Major revisions to Trade-In page and site.css. Added scripts to site.js.

--- Final---Magix/Final---Magix/wwwroot/js/site.js
+++ Final---Magix/Final---Magix/wwwroot/js/site.js
@@ -1,19 +1,35 @@
 ﻿function openTooltip(id) {
-	var tooltip = document.getElementById(id);
-	tooltip.style.display = "block";
-	tooltip.classList.add('display-block'); // Add this line
-}
+	var tooltips = document.getElementsByClassName('tooltip-modal');
+	for (var i = 0; i < tooltips.length; i++) {
+		tooltips[i].style.display = 'none';
+	}
 
+	var tooltip = document.getElementById(id);
+	var icon = document.querySelector('.tooltip-icon[onclick="openTooltip(\'' + id + '\')"]');
+	var rect = icon.getBoundingClientRect();
+
+	tooltip.style.left = rect.left + 'px';
+	tooltip.style.top = rect.top + rect.height + 'px'; // Adjust this value to position the tooltip
+
+	tooltip.style.display = "block";
+	tooltip.classList.add('display-block');
+}
 
 function closeTooltip(id) {
 	document.getElementById(id).style.display = "none";
 }
 
+function closeAllTooltips() {
+	var tooltips = document.getElementsByClassName('tooltip-modal');
+	for (var i = 0; i < tooltips.length; i++) {
+		tooltips[i].style.display = 'none';
+	}
+}
+
 window.onclick = function (event) {
 	if (!event.target.matches('.tooltip-icon')) {
-		var tooltips = document.getElementsByClassName('tooltip-modal');
-		for (var i = 0; i < tooltips.length; i++) {
-			tooltips[i].style.display = 'none';
-		}
+		closeAllTooltips();
 	}
 };
+
+window.addEventListener('scroll', closeAllTooltips);
