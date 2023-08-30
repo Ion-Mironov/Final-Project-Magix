9a7db9973f528e45698cf81552e3fdcfb61101c9
Ivon Mironov <mironov.ion@gmail.com>
2023-08-30 11:52:03 -0400

    Formatted InventoryModel.cs for better readability.

--- Final---Magix/Final---Magix/Models/InventoryModel.cs
+++ Final---Magix/Final---Magix/Models/InventoryModel.cs
@@ -2,25 +2,20 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Final___Magix.Models
-{
-    public class Inventory
-    {
+	{
+	public class Inventory
+		{
+		[NotMapped]
+		public string? Object { get; set; }
 
-        [NotMapped]
-        public string? Object { get; set; }
-        [Key]
-        public string? Id { get; set; }
-        public string? Name { get; set; }
-
-        public string? ImageSmall { get; set; }
-        public string? ImageNormal { get; set; }
-        public string? ImageLarge { get; set; }
-        public string? ImageBorderCrop { get; set; }
-
-		
+		[Key]
+		public string? Id { get; set; }
+		public string? Name { get; set; }
+		public string? ImageSmall { get; set; }
+		public string? ImageNormal { get; set; }
+		public string? ImageLarge { get; set; }
+		public string? ImageBorderCrop { get; set; }
 		public decimal? Price { get; set; }
-
-        public int? Quantity { get; set; }
-    }
-
-}
+		public int? Quantity { get; set; }
+		}
+	}
