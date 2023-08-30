75394841d32444047454496245f7585d8233f83b
Ivon Mironov <mironov.ion@gmail.com>
2023-08-30 11:23:08 -0400

    Formatted TradeInController.cs for better readability.

--- Final---Magix/Final---Magix/Controllers/TradeInController.cs
+++ Final---Magix/Final---Magix/Controllers/TradeInController.cs
@@ -1,106 +1,95 @@
 ﻿using Final___Magix.DataContext;
-using Final___Magix.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Final___Magix.Controllers
-{
-
-	public class TradeInController : Controller
 	{
+	public class TradeInController : Controller
+		{
 		private readonly CardContext _dbContext;
 		public TradeInController(CardContext dbContext)
-		{
+			{
 			_dbContext = dbContext;
-		}
+			}
 
 		public ActionResult Index()
 		// GET: TradeInController
-		{
-
+			{
 			var tradein = _dbContext.TradeIns.ToList();
 			return View(tradein);
-		}
+			}
 
 		// GET: TradeInController/Details/5
 		public ActionResult Details(int id)
-		{
+			{
 			return View();
-		}
+			}
 
 		// GET: TradeInController/Create
-		//*//
-
 		[HttpGet]
 		public IActionResult Create()
-		{
+			{
 			return View();
-		}
-
+			}
 
 		// POST: TradeInController/Create
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		public ActionResult Create(IFormCollection collection)
-		{
+			{
 			try
-			{
-				//fetch matching card data and add it to the trade-in
-
-
-				ViewBag.BulkDataEntries = _dbContext.BulkData.ToList();
+				{
+				ViewBag.BulkDataEntries = _dbContext.BulkData.ToList();		// Fetch matching card data and add it to the trade-in
 				return View();
-			}
+				}
 			catch
-			{
+				{
 				return View();
+				}
 			}
-		}
 
 		// GET: TradeInController/Delete/5
 		public ActionResult Delete(int id)
-		{
+			{
 			return View();
-		}
+			}
 
 		// POST: TradeInController/Delete/5
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		public ActionResult Delete(int id, IFormCollection collection)
-		{
+			{
 			try
-			{
+				{
 				return RedirectToAction(nameof(Index));
-			}
+				}
 			catch
-			{
+				{
 				return View();
+				}
 			}
-		}
+
 		// Get all Name properties from the BulkData db table and store them in a list
 		public IActionResult ValidateCardName(string cardName)
-		{
+			{
 			var bulkDataNames = _dbContext.BulkData.Select(data => data.Name).ToList();
 			var isValid = bulkDataNames.Contains(cardName);
 			return Ok(new { isValid = isValid });
-		}
-
+			}
 
 		[HttpGet]
 		public IActionResult GetMatchingCards(string cardName)
-		{
+			{
 			var matchingCardNames = _dbContext.BulkData
 				.Where(card => card.Name.ToLower().Contains(cardName))
 				.ToList();
 
 			return Json(new { matchingCardNames });
-		}
+			}
 
 		//[HttpPost]
 		//public IActionResult CreateTradeIn(object[] cards)
 		//{
-		//	//convert array of card objects into the proper type to store in the tradein db
+		//	// convert array of card objects into the proper type to store in the tradein db
 		//	List<CardModel> _cards = new();
 		//	foreach (var obj in cards)
 		//	{
@@ -123,7 +112,7 @@ namespace Final___Magix.Controllers
 		//	{
 		//		using (var connection = _dbContext.Database.GetDbConnection())
 		//		{
-		//			//Some wizardry... Don't ask.
+		//			// Some wizardry... Don't ask.
 		//			connection.Open();
 		//			using (var cmd = connection.CreateCommand())
 		//			{
@@ -136,7 +125,7 @@ namespace Final___Magix.Controllers
 		//			{
 		//				Id = totalTradeInEntries + 1,
 		//				Cards = _cards
-  //                  };
+		//                  };
 		//			_dbContext.TradeIns.Add(tradeIn);
 		//			_dbContext.SaveChanges();
 
@@ -154,5 +143,7 @@ namespace Final___Magix.Controllers
 		//		return RedirectToAction("Error", "Home");
 		//	}
 		//}
-	}
-}
+
+		}	// Closing public class TradeInController : Controller
+
+	}   // Closing namespace Final___Magix.Controllers
